8522232fd8fff7ca4066773e70c56cfc010633f2
schulteiss20 <143538995+schulteiss20@users.noreply.github.com>
2023-10-31 13:45:47 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,53 @@
-from flask import Flask, render_template, request
+from flask import Flask
 import psycopg2
+import util
 
 app = Flask(__name__)
 
 # PostgreSQL connection settings
 db_settings = {
-    'dbname': 'dvdrental',
+    'dbname': 'postgres',
     'user': 'raywu1990',
     'password': 'test',
     'host': '127.0.0.1',
-    'port': '5432'
+    'port': '5432',
 }
+
+def connect_to_db():
+    try:
+        conn = psycopg2.connect(**db_settings)
+        cursor = conn.cursor()
+        return cursor, conn
+    except psycopg2.Error as e:
+        raise e
+
+def disconnect_from_db(cursor, conn):
+    try:
+        cursor.close()
+        conn.close()
+    except Exception as e:
+        raise e
 
 @app.route('/api/update_basket_a')
 def update_basket_a():
     try:
-        conn = psycopg2.connect(**db_settings)
-        cursor = conn.cursor()
+        cursor, connection = connect_to_db()
 
         # Insert a new row into basket_a
         cursor.execute("INSERT INTO basket_a (a, fruit_a) VALUES (5, 'Cherry');")
-        conn.commit()
+        connection.commit()
 
         return "Success!"
     except psycopg2.Error as e:
         error_message = str(e)
         return f"Error: {error_message}"
     finally:
-        cursor.close()
-        conn.close()
+        disconnect_from_db(cursor, connection)
 
 @app.route('/api/unique')
 def unique_fruits():
     try:
-        conn = psycopg2.connect(**db_settings)
-        cursor = conn.cursor()
+        cursor, conn = connect_to_db()
 
         # Fetch unique fruits from basket_a and basket_b
         cursor.execute("SELECT DISTINCT fruit_a FROM basket_a UNION SELECT DISTINCT fruit_b FROM basket_b;")
@@ -51,8 +64,7 @@ def unique_fruits():
         error_message = str(e)
         return f"Error: {error_message}"
     finally:
-        cursor.close()
-        conn.close()
+        disconnect_from_db(cursor, conn)
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000)
